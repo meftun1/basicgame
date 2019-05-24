894f8c21d6f902c9f7c6d0fa1e29ace8ae833d2a
Emirhan Yılmaz <45498623+meftun1@users.noreply.github.com>
2019-05-24 14:24:46 +0300

    Initial commit

---
+++ Assets/Codes/hero.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hero : MonoBehaviour
+{
+ 
+    Rigidbody2D rb_char;
+    public float movementSpeed,jump,maxspeed;
+    Animator animate;
+    public bool onFloor;
+    void Start()
+    {
+        animate = GetComponent<Animator>();
+        rb_char = GetComponent<Rigidbody2D>();
+        
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        rb_char.AddForce(Vector3.right * h * movementSpeed);
+        animate.SetBool("onfloor",onFloor);
+        animate.SetFloat("speed",Mathf.Abs(h));
+        if (h>0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        if (h<0)
+        {
+            transform.localScale = new Vector2(1,1);
+        }
+        if (rb_char.velocity.x>maxspeed)
+        {
+            rb_char.velocity = new Vector2(maxspeed,rb_char.velocity.y);
+        }
+        if (rb_char.velocity.x < -maxspeed)
+        {
+            rb_char.velocity = new Vector2(-maxspeed, rb_char.velocity.y);
+        }
+    }
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Codes/onfloortrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onfloortrigger : MonoBehaviour
+{
+    hero karakter;
+    void Start()
+    {
+        karakter = transform.root.gameObject.GetComponent<hero>();
+    }
+
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D()
+    {
+        karakter.onFloor = true;
+    }
+    void OnTriggerStay2D()
+    {
+        karakter.onFloor = true;
+    }
+     void OnTriggerExit2D()
+    {
+        karakter.onFloor = false;
+    }
+
+}
